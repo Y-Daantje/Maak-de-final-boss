9dd0a1e596cdab2788235fb25e3feb2cc1e5a849
Y-Daantje <302867649@student.rocmondriaan.nl>
2024-02-01 15:15:15 +0100

    niet klaar

---
+++ main.js
@@ -0,0 +1,12 @@
+fetch('gods.json')
+.then(myData => myData.Json())
+.then(myData1 => showInConsole(JsonData));
+console.log(myData1);
+
+
+function createCard() {
+    for (let i = 0; i < array.length; i++) {
+        const card = array[i];
+        
+    }
+}
